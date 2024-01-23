16e43e6e0d0e2e1fd539f1e0aa30c10d8b938e70
KLCALAW <ichinocalvin@hotmail.nl>
2024-01-23 14:40:41 +0100

    initial modules.py where global values & (some) definitions have been added

---
+++ modules.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Number of market participants 
+N = 1600 
+
+# Number of fundamentalists and chartists
+N_FUND = 800
+N_CHART = 800
+
+# Initial price of the asset traded
+P_0 = 100
+
+# Global fundamental price 
+P_F = 120
+
+# Variation around the global fundamental price 
+THETA = 30
+
+# Maximum extension for chartists' timesteps window 
+T_MAX = 15
+
+# Sensitivity of forecasts for fundamentalists
+PHI = 2.0
+
+# Sensitivity of forecasts for chartists 
+KAPPA = 2.0
+
+# level of information dissipation among the agents
+ALPHA = 0.95
+
+# Weight of the market imbalance
+DELTA = 0.05
+
+# Intervals of variation for the stochastic noise term 
+SIGMA = 30
+
+# Sensitivity threshold for choosing the status
+TAO = 20
+
+# Asset quantity for each trader
+Q = 50
+
+# Initializing traders' wealth (W), money (M), and asset quantity(Q)
+M = 35000  
+Q = 50 
+W = [M + Q * P_0 for _ in range(N)] 
+
+# Function to calculate expected price for fundamentalists
+def ep_fundamentalist(p_t, P_F)
+    """
+    Calculate the expected price for a fundamentalist trader
+
+    Parameters:
+    p_t (float): The current market price of the asset
+    p_f (float): The perceived fundamental price of the asset for the trader
+
+    Returns:
+    float: The expected price for the asset for a fundamentalist trader
+    """
+    noise = np.random.uniform(-SIGMA, SIGMA)
+    p_pf = P_F
+    return p_t + PHI * (p_f - p_t) + noise
+
+## TODO: 
+"""
+Implement the following for chartists where T is randomized:
+
+A chartist decides her behavior according to her inspection of past prices. Therefore, the expected price of each
+trader belonging to this group is a function of past prices:
+in particular we adopt the average of last T prices over
+a time window that is different for each chartist (T ∈ [2,
+Tmax]).
+"""
+
+def expected_price_chartist(p_t, past_prices):
+    """
+    Calculate the expected price for a chartist trader
+
+    Parameters:
+    p_t (float): The current market price of the asset
+    past_prices (list of float): A list of past market prices of the asset
+
+    Returns:
+    float: The expected price for the asset as calculated by a chartist trader
+    """
+    p_T = np.mean(past_prices[:T_MAX]) 
+    noise = random.uniform(-SIGMA, SIGMA)
+    return p_t + KAPPA * (p_t - p_T) + noise
+
